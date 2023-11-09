ac40e1300176c2ba1caa7cc6204c67939c31edb9
jamsheeraslam <jamsheeraslam4269@gmail.com>
2023-11-08 23:25:45 -0600

    Implement list all

--- controllers/phone.js
+++ controllers/phone.js
@@ -1,21 +1,28 @@
 var phone = require('../models/phone');
 // List of all Phones
-exports.phone_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Phone list');
-};
+exports.phone_list = async function (req, res) {
+    try {
+        thePhone = await phone.find();
+        res.send(thePhone);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+}
 // for a specific Phone.
-exports.phone_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Phone detail: ' + req.params.id);
+exports.phone_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: Phone detail: ' + req.params.id);
 };
 // Handle Phone create on POST.
-exports.phone_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Phone create POST');
+exports.phone_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: Phone create POST');
 };
 // Handle Phone delete form on DELETE.
-exports.phone_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Phone delete DELETE ' + req.params.id);
+exports.phone_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: Phone delete DELETE ' + req.params.id);
 };
 // Handle Phone update form on PUT.
-exports.phone_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Phone update PUT' + req.params.id);
+exports.phone_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: Phone update PUT' + req.params.id);
 };
