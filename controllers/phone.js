f9f0fba94f89b20fb04c922430a781b664929221
jamsheeraslam <jamsheeraslam4269@gmail.com>
2023-11-10 11:48:35 -0600

    modified

--- controllers/phone.js
+++ controllers/phone.js
@@ -10,10 +10,17 @@ exports.phone_list = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 }
-// for a specific Phone.
-exports.phone_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Phone detail: ' + req.params.id);
-};
+// for a specific Phone..
+exports.phone_detail = async function(req, res) {
+  console.log("detail" + req.params.id)
+  try {
+  result = await phone.findById( req.params.id)
+  res.send(result)
+  } catch (error) {
+  res.status(500)
+  res.send(`{"error": document for id ${req.params.id} not found`);
+  }
+  };
 // Handle Phone create on POST.
 exports.phone_create_post =  async function (req, res) {
     console.log(req.body);
@@ -21,7 +28,7 @@ exports.phone_create_post =  async function (req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"phone_type":"goat", "cost":12, "size":"large"}
     document.phone_name = req.body.phone_name;
     document.phone_color = req.body.phone_color;
     document.phone_cost = req.body.phone_cost;
@@ -37,9 +44,25 @@ exports.phone_create_post =  async function (req, res) {
 exports.phone_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Phone delete DELETE ' + req.params.id);
 };
-// Handle Phone update form on PUT.
-exports.phone_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Phone update PUT' + req.params.id);
+// Handle update form on PUT.
+exports.phone_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await phone.findById( req.params.id)
+// Do updates of properties
+if(req.body.phone_name)
+toUpdate.phone_name = req.body.phone_name;
+if(req.body.phone_color) toUpdate.phone_color = req.body.phone_color;
+if(req.body.phone_cost) toUpdate.phone_cost = req.body.phone_cost;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
 exports.phone_view_all_Page = async function (req, res) {
     try {
--- routes/phone.js
+++ routes/phone.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const phone_controlers= require('../controllers/phone');
 var router = express.Router();
-/* GET costumes */
-router.get('/', phone_controlers.phone_view_all_Page );
+/* GET request for one costume. */
+router.get('/phone/:id', phone_controlers.phone_detail);
 module.exports = router
