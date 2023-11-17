ab977af00a2c013f29c8f2bdd714adfab5437af7
jamsheeraslam <jamsheeraslam4269@gmail.com>
2023-11-17 11:24:51 -0600

    added account.js

--- routes/phone.js
+++ routes/phone.js
@@ -3,7 +3,7 @@ const phone_controlers= require('../controllers/phone');
 var router = express.Router();
 /* GET request for one phone. */
 router.get('/phone/:id', phone_controlers.phone_detail);
-
+router.get('/', phone_controlers.phone_view_all_Page );
 /* GET detail phone page */
 router.get('/detail', phone_controlers.phone_view_one_Page);
 
