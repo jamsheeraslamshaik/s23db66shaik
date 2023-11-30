3ddccadb587f21dbf95c022b1aa59ca8b059ad21
jamsheeraslam <jamsheeraslam4269@gmail.com>
2023-11-29 22:04:28 -0600

    modified

--- models/phone.js
+++ models/phone.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const phoneSchema = mongoose.Schema({
-    phone_name: String,
-    phone_color: String,
-    phone_cost: Number
+    phone_name:  { type: String, minlength: 3 },
+    phone_color: { type: String, required: true },
+    phone_cost: { type: Number, required: true }
 })
 module.exports = mongoose.model("Phone",
 phoneSchema)
--- routes/phone.js
+++ routes/phone.js
@@ -1,6 +1,12 @@
 var express = require('express');
 const phone_controlers= require('../controllers/phone');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    res.redirect("/login");
+  };
 /* GET request for one phone. */
 router.get('/phone/:id', phone_controlers.phone_detail);
 router.get('/', phone_controlers.phone_view_all_Page );
@@ -8,11 +14,11 @@ router.get('/', phone_controlers.phone_view_all_Page );
 router.get('/detail', phone_controlers.phone_view_one_Page);
 
 /* GET create phone page */
-router.get('/create', phone_controlers.phone_create_Page);
+router.get('/create',secured, phone_controlers.phone_create_Page);
 
 /* GET create update page */
-router.get('/update', phone_controlers.phone_update_Page);
+router.get('/update', secured, phone_controlers.phone_update_Page);
 
 /* GET delete phone page */
-router.get('/delete', phone_controlers.phone_delete_Page);
+router.get('/delete', secured, phone_controlers.phone_delete_Page);
 module.exports = router
